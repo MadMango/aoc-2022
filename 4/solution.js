7f032ec836aba9bc26a87ac6320a9510b0170ca9
madmango <verymadmango@gmail.com>
2022-12-05 18:57:45 +0100

    day 4

---
+++ 4/solution.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("input.txt", {
+  encoding: "utf8",
+  flag: "r",
+});
+const pairs = input.split(/\n/);
+
+function isBetween(x, min, max) {
+  return x >= min && x <= max;
+}
+
+const containedFully = pairs.filter((pair) => {
+  const [left, right] = pair.split(",");
+  const [leftMin, leftMax] = left.split("-").map(Number);
+  const [rightMin, rightMax] = right.split("-").map(Number);
+
+  const leftMinFits =
+    isBetween(leftMin, rightMin, rightMax) && leftMin >= rightMin;
+  const leftMaxFits = isBetween(leftMax, rightMin, rightMax);
+
+  const rightMinFits =
+    isBetween(rightMin, rightMin, rightMax) && rightMin >= leftMin;
+  const rightMaxFits = isBetween(rightMax, leftMin, leftMax);
+
+  const leftFits = leftMinFits && leftMaxFits;
+  const rightFits = rightMinFits && rightMaxFits;
+
+  return leftFits || rightFits;
+});
+
+console.info(containedFully.length); // 441
+
+// PART 2
+
+const overlaps = pairs.filter((pair) => {
+  const [left, right] = pair.split(",");
+  const [leftMin, leftMax] = left.split("-").map(Number);
+  const [rightMin, rightMax] = right.split("-").map(Number);
+
+  const leftOverlap =
+    isBetween(leftMin, rightMin, rightMax) ||
+    isBetween(leftMax, rightMin, rightMax);
+
+  const rightOverlap =
+    isBetween(rightMin, leftMin, leftMax) ||
+    isBetween(rightMax, leftMin, leftMax);
+
+  return leftOverlap || rightOverlap;
+});
+
+console.info(overlaps.length);
